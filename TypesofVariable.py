5a04b78434e0cd84e4bd27cdb27a677a68a45c9f
DannyAbadMEC <71148518+DannyAbadMEC@users.noreply.github.com>
2021-07-21 18:11:59 -0500

    Update TypesofVariable.py

---
+++ TypesofVariable.py
@@ -0,0 +1,41 @@
+x = 'hello'
+print(x) #string
+x = 20
+print(x) #int
+x = 20.5
+print(x) #float
+x = 1j
+print(x) #complex
+x = ['apple', 'banana', 'cherry']
+print(x) #list
+x = 'hello'
+print(x)
+x = ('apple', 'banana', 'cherry')
+print(x) #tuple
+x = range(6)
+print(x) #range
+x = {"name":"abad", "age":19}
+print(x) #dict
+x = {'apple', 'banana', 'cherry'}
+print(x) #set
+x = frozenset ({'apple', 'banana', 'cherry'})
+print(x) #frozenset
+x = True
+print(x) #boolean
+x = b"bytes"
+print(x) #bytes
+x = bytearray (5)
+print(x) #bytearray
+x = memoryview(bytes (6))
+print(x) #memoryview
+
+print(type (100))
+
+name, last_name = 'danny', 'Adan'
+print(name, last_name)
+
+Str_experiment = 'Danny Abad'
+(dir(Str_experiment))
+
+Lista = []
+a = input(print('ingrese datos'))
