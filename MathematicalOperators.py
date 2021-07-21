8ba8abf21a3ccda28176039b5aa1730aa302dd88
DannyAbadMEC <71148518+DannyAbadMEC@users.noreply.github.com>
2021-07-21 18:09:41 -0500

    Update MathematicalOperators.py

---
+++ MathematicalOperators.py
@@ -0,0 +1,27 @@
++ ---- OPERADOR DE SUMA
+- ---- OPERADOR DE RESTA
+- ---- OPERADOR NEGATIVO
+* ---- OPERADOR DE MULTIPLICACION
+** --- OPERADPR DE POTENCIA
+/ ---- OPERACION DE DIVISION
+// --- OPERACION DE DIVISION ENTERA
+% ---- OPERADOR DE MODULO O RESTO
+
+"""
+# Reglas de procedencia de las operaciones
+"""
+1._ Parentesis
+2._ Exponentes
+3._ Multiplicacion
+4._ Division
+5._ Suma
+
+"""
+x = 5 * 8 / 2 - 10 ** 2
+print(x)
+
+z = (5 * 8) / 2 - 10 ** 2
+print(z)
+
+h = 5 * (8 / 2) - 10 ** 2
+print(h)
